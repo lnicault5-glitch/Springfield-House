298f777697c014da82ecf1c98b1ee35dafbe2614
Laurent Nicault <laurentnicault@Laurents-MacBook-Pro.local>
2025-10-30 12:59:50 -0400

    Add STATIC_ROOT for production deployment

--- springfield/settings.py
+++ springfield/settings.py
@@ -122,5 +122,7 @@ TEMPLATES[0]['DIRS'] = [
 ]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Path where static files will be collected for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
